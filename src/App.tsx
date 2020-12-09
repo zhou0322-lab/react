4974135f35983b286095968ca905e28a198ef21e
dengjun <dengjun@staff.easub.com>
2020-12-09 18:44:16 +0800

    '添加路由'

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,46 @@ import { Button, Input } from "antd";
 import bookIcon from "./images/book_icon.png";
 import { Helmet } from "react-helmet";
 import { ReactComponent as Logo } from "./images/common_icon.svg";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const Test = () => {
+  return <div>aaaa</div>;
+};
+
+const TestB = () => {
+  return <div>bbbb</div>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <Helmet>
-        <title>测试react</title>
-      </Helmet>
-      <img src={bookIcon} alt="" />
-      <Button type="primary">antd</Button>
-      <Input placeholder="aaa" />
-      <Logo />
-    </div>
+    <Router>
+      <div className="App">
+        <Helmet>
+          <title>测试react</title>
+        </Helmet>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/a">Home</Link>
+            </li>
+            <li>
+              <Link to="/b">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <img src={bookIcon} alt="" />
+        <Button type="primary">antd</Button>
+        <Input placeholder="aaa" />
+        <Logo />
+        <Switch>
+          <Route path="/a" component={Test} />
+          <Route path="/b" component={TestB} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
