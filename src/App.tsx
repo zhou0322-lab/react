8c9ac207e5804f89ef2ed05d6863e0404a2bed06
dengjun <dengjun@staff.easub.com>
2020-12-09 13:56:38 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+import { Button } from "./stories/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button label="test" />
+    </div>
+  );
+}
+
+export default App;
