f865725d8406206b0b44c69922b3d3b04d275807
dengjun <dengjun@staff.easub.com>
2020-12-11 18:41:15 +0800

    '路由'

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,64 @@
 import React from "react";
 import "./App.less";
-import { Button, Input } from "antd";
-import bookIcon from "./images/book_icon.png";
-import { Helmet } from "react-helmet";
-import { ReactComponent as Logo } from "./images/common_icon.svg";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Button } from "antd";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useHistory,
+  useLocation,
+  useRouteMatch,
+} from "react-router-dom";
 
-const Test = () => {
-  return <div>aaaa</div>;
-};
+// Params are placeholders in the URL that begin
+// with a colon, like the `:id` param defined in
+// the route in this example. A similar convention
+// is used for matching dynamic segments in other
+// popular web frameworks like Rails and Express.
 
-const TestB = () => {
-  return <div>bbbb</div>;
-};
-
-function App() {
+export default function ParamsExample() {
   return (
     <Router>
-      <div className="App">
-        <Helmet>
-          <title>测试react</title>
-        </Helmet>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/a">Home</Link>
-            </li>
-            <li>
-              <Link to="/b">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-        <img src={bookIcon} alt="" />
-        <Button type="primary">antd</Button>
-        <Input placeholder="aaa" />
-        <Logo />
+      <div>
+        <h2>Accounts</h2>
+
+        <ul>
+          <li>
+            <Link to="/netflix">Netflix</Link>
+          </li>
+          <li>
+            <Link to="/yahoo">Yahoo</Link>
+          </li>
+          <li>
+            <Link to="/modus-create">Modus Create</Link>
+          </li>
+        </ul>
+
         <Switch>
-          <Route path="/a" component={Test} />
-          <Route path="/b" component={TestB} />
+          <Route path="/:id" children={<Child />} />
         </Switch>
       </div>
     </Router>
   );
 }
 
-export default App;
+function Child() {
+  // We can use the `useParams` hook here to access
+  // the dynamic pieces of the URL.
+  const { id }: any = useParams();
+  const history = useHistory();
+  console.log(useLocation(), useRouteMatch());
+  const go = () => {
+    history.push("/Zillow Group");
+  };
+  return (
+    <div>
+      <Button onClick={go} type="primary">
+        Zillow Group
+      </Button>
+      <h3>ID: {id}</h3>
+    </div>
+  );
+}
